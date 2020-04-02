4c32fd45b0f464a9465632ea6b72cbe4b1b57eed
Ian Chu <chu2815@gmail.com>
2020-04-02 22:35:07 +0800

    Add google puppeteer case

---
+++ src/google.js
@@ -0,0 +1,24 @@
+const puppeteer = require("puppeteer");
+const devices = require("puppeteer/DeviceDescriptors");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+  await page.emulate(devices["iPhone 7"]);
+  await page.goto("https://google.com");
+  await page.screenshot({ path: "./screenshot_google.png" });
+  await page.evaluate(() => {
+    document.querySelector("input[type='search']").value = "corona virus";
+    document.querySelector("button[jsaction='click:.CLIENT']").click();
+  });
+
+  await page.waitForNavigation();
+  await page.evaluate(() => {
+    document.querySelector('div[role="heading"]').click();
+  });
+  await page.waitForNavigation();
+  await page.screenshot({ path: "./screenshot_google_search.png" });
+  await browser.close();
+})();
